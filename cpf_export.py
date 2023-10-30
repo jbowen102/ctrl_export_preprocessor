e1794b63c9880b444eebaa40d2b78b9d4e4e6d0c
JB (work) <ew15dro6k216@opayq.net>
2023-10-29 21:29:23 -0400

    Minimize mouse movement reliance. Change to mostly key strokes. Create dir_names.py.

--- cpf_export.py
+++ cpf_export.py
@@ -1,33 +1,55 @@
 import os
+import time
+
 import pyautogui as gui
 
+from dir_names import CPF_DIR, IMPORT_DIR, EXPORT_DIR
 
-FILE_BUTTON_X_POS=978;
-FILE_BUTTON_Y_POS=70+47;
+
+# Constants
+PROG_POS_X=1433
+PROG_POS_Y=547
+
+
 
 def export_cpfs():
-    gui.moveTo(FILE_BUTTON_X_POS, FILE_BUTTON_Y_POS);
-    gui.click(); # Press File button in toolbar
-    gui.moveRel(29, 20);
-    gui.click(); # Press Open
-    gui.moveRel(236, 164, .5);
-    gui.click(); # Select file to import.
-    gui.hotkey("f2"); # Rename shortcut
-    gui.hotkey("ctrl", "a"); # Highlight whole filename, including extension
-    gui.hotkey("ctrl", "c"); # Copy filename, including extension
+    gui.click(PROG_POS_X, PROG_POS_Y) # Click on program to bring into focus
+
+    # Open file
+    gui.hotkey("ctrl", "o")
+    gui.hotkey("ctrl", "l") # Select address bar
+    gui.typewrite(IMPORT_DIR) # Navigate to import folder.
+    gui.press(["enter"])
+
+    # Copy filename to clipboard for use in export.
+    gui.click(1207, 209); # Select first file in CPF_DIR to import.
+    gui.hotkey("f2"); # "Rename" shortcut
+    gui.hotkey("ctrl", "c"); # Copy filename, excluding extension
     gui.press(["esc"]), # Exit rename
-    gui.doubleClick(); # Confirm file to open
-    gui.moveTo(FILE_BUTTON_X_POS, FILE_BUTTON_Y_POS, .5);
-    gui.click(); # Press File button in toolbar again
-    gui.moveRel(65, 113, .2);
-    gui.click(); # Click Export
-    gui.hotkey("ctrl", "v"); # Paste in imported filename (includes extension)
-    gui.press(["backspace"]*4) # Remove extension and 
-    gui.press(["Enter"]) # Confirm export filename
+
+    gui.press(["enter"]) # Confirm CPF filename to open.
+    time.sleep(1) # Allow time for CPF to open.
+
+    # Export
+    gui.hotkey("alt", "f") # Open File menu (toolbar).
+    gui.press(["e"]) # Select Export from File menu.
+    gui.hotkey("ctrl", "v"); # Paste in imported filename
+
+    gui.hotkey("ctrl", "l") # Select address bar
+    gui.typewrite(EXPORT_DIR) # Navigate to export folder.
+    gui.press(["enter"])
+    gui.hotkey("alt", "s") # Save
 
 
 if __name__ == "__main__":
+    # Make CPF dirs if any don't exist yet.
+    if not os.path.exists(CPF_DIR):
+        os.mkdir(CPF_DIR)
+    if not os.path.exists(IMPORT_DIR):
+        os.mkdir(IMPORT_DIR)
+    if not os.path.exists(EXPORT_DIR):
+        os.mkdir(EXPORT_DIR)
+
+    input("Ready for GUI interaction?")
     export_cpfs()
-    print("done")
-
-
+    print("GUI interaction done")
