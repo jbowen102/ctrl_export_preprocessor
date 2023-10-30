4afa1e71bdf09971b606bd63b0d091f340b6d442
JB (work) <ew15dro6k216@opayq.net>
2023-10-29 21:41:37 -0400

    Close previously-opened CPF between exports. Add more sleeps to increase reliability, especially on first program start.

--- cpf_export.py
+++ cpf_export.py
@@ -17,28 +17,44 @@ def export_cpfs():
 
     # Open file
     gui.hotkey("ctrl", "o")
+    time.sleep(0.2)
     gui.hotkey("ctrl", "l") # Select address bar
+    time.sleep(0.2)
     gui.typewrite(IMPORT_DIR) # Navigate to import folder.
+    time.sleep(0.2)
     gui.press(["enter"])
+    time.sleep(0.2)
 
     # Copy filename to clipboard for use in export.
     gui.click(1207, 209); # Select first file in CPF_DIR to import.
     gui.hotkey("f2"); # "Rename" shortcut
+    time.sleep(0.2)
     gui.hotkey("ctrl", "c"); # Copy filename, excluding extension
+    time.sleep(0.2)
     gui.press(["esc"]), # Exit rename
+    time.sleep(0.2)
 
     gui.press(["enter"]) # Confirm CPF filename to open.
     time.sleep(1) # Allow time for CPF to open.
 
     # Export
     gui.hotkey("alt", "f") # Open File menu (toolbar).
+    time.sleep(0.2)
     gui.press(["e"]) # Select Export from File menu.
+    time.sleep(0.2)
     gui.hotkey("ctrl", "v"); # Paste in imported filename
+    time.sleep(0.2)
 
     gui.hotkey("ctrl", "l") # Select address bar
+    time.sleep(0.1)
     gui.typewrite(EXPORT_DIR) # Navigate to export folder.
+    time.sleep(0.1)
     gui.press(["enter"])
+    time.sleep(0.2)
     gui.hotkey("alt", "s") # Save
+
+    time.sleep(0.5)
+    gui.hotkey("ctrl", "f4") # Close CPF file.
 
 
 if __name__ == "__main__":
